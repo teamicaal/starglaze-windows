82cc9e2f3962cf94b76a2f299e5d9b33a21d89d7
Matt Pickles <mattwpickles@gmail.com>
2023-08-08 17:56:31 +0100

    Package updates

--- web/app/cache/acorn/framework/cache/services.php
+++ web/app/cache/acorn/framework/cache/services.php
@@ -3,30 +3,34 @@
   array (
     0 => 'Illuminate\\Bus\\BusServiceProvider',
     1 => 'Illuminate\\Cache\\CacheServiceProvider',
-    2 => 'Illuminate\\Pipeline\\PipelineServiceProvider',
-    3 => 'Illuminate\\Routing\\RoutingServiceProvider',
-    4 => 'Illuminate\\Session\\SessionServiceProvider',
-    5 => 'Roots\\Acorn\\Assets\\AssetsServiceProvider',
-    6 => 'Roots\\Acorn\\Filesystem\\FilesystemServiceProvider',
-    7 => 'Roots\\Acorn\\Providers\\AcornServiceProvider',
-    8 => 'Roots\\Acorn\\Providers\\RouteServiceProvider',
-    9 => 'Roots\\Acorn\\View\\ViewServiceProvider',
-    10 => 'Carbon\\Laravel\\ServiceProvider',
-    11 => 'Log1x\\SageSvg\\SageSvgServiceProvider',
-    12 => 'App\\Providers\\ThemeServiceProvider',
+    2 => 'Illuminate\\Database\\DatabaseServiceProvider',
+    3 => 'Illuminate\\Database\\MigrationServiceProvider',
+    4 => 'Illuminate\\Foundation\\Providers\\ComposerServiceProvider',
+    5 => 'Illuminate\\Pipeline\\PipelineServiceProvider',
+    6 => 'Illuminate\\Routing\\RoutingServiceProvider',
+    7 => 'Illuminate\\Session\\SessionServiceProvider',
+    8 => 'Roots\\Acorn\\Assets\\AssetsServiceProvider',
+    9 => 'Roots\\Acorn\\Filesystem\\FilesystemServiceProvider',
+    10 => 'Roots\\Acorn\\Providers\\AcornServiceProvider',
+    11 => 'Roots\\Acorn\\Providers\\RouteServiceProvider',
+    12 => 'Roots\\Acorn\\View\\ViewServiceProvider',
+    13 => 'Carbon\\Laravel\\ServiceProvider',
+    14 => 'Log1x\\SageSvg\\SageSvgServiceProvider',
+    15 => 'App\\Providers\\ThemeServiceProvider',
   ),
   'eager' => 
   array (
-    0 => 'Illuminate\\Routing\\RoutingServiceProvider',
-    1 => 'Illuminate\\Session\\SessionServiceProvider',
-    2 => 'Roots\\Acorn\\Assets\\AssetsServiceProvider',
-    3 => 'Roots\\Acorn\\Filesystem\\FilesystemServiceProvider',
-    4 => 'Roots\\Acorn\\Providers\\AcornServiceProvider',
-    5 => 'Roots\\Acorn\\Providers\\RouteServiceProvider',
-    6 => 'Roots\\Acorn\\View\\ViewServiceProvider',
-    7 => 'Carbon\\Laravel\\ServiceProvider',
-    8 => 'Log1x\\SageSvg\\SageSvgServiceProvider',
-    9 => 'App\\Providers\\ThemeServiceProvider',
+    0 => 'Illuminate\\Database\\DatabaseServiceProvider',
+    1 => 'Illuminate\\Routing\\RoutingServiceProvider',
+    2 => 'Illuminate\\Session\\SessionServiceProvider',
+    3 => 'Roots\\Acorn\\Assets\\AssetsServiceProvider',
+    4 => 'Roots\\Acorn\\Filesystem\\FilesystemServiceProvider',
+    5 => 'Roots\\Acorn\\Providers\\AcornServiceProvider',
+    6 => 'Roots\\Acorn\\Providers\\RouteServiceProvider',
+    7 => 'Roots\\Acorn\\View\\ViewServiceProvider',
+    8 => 'Carbon\\Laravel\\ServiceProvider',
+    9 => 'Log1x\\SageSvg\\SageSvgServiceProvider',
+    10 => 'App\\Providers\\ThemeServiceProvider',
   ),
   'deferred' => 
   array (
@@ -40,6 +44,18 @@
     'cache.psr6' => 'Illuminate\\Cache\\CacheServiceProvider',
     'memcached.connector' => 'Illuminate\\Cache\\CacheServiceProvider',
     'Illuminate\\Cache\\RateLimiter' => 'Illuminate\\Cache\\CacheServiceProvider',
+    'migrator' => 'Illuminate\\Database\\MigrationServiceProvider',
+    'migration.repository' => 'Illuminate\\Database\\MigrationServiceProvider',
+    'migration.creator' => 'Illuminate\\Database\\MigrationServiceProvider',
+    'Illuminate\\Database\\Console\\Migrations\\MigrateCommand' => 'Illuminate\\Database\\MigrationServiceProvider',
+    'Illuminate\\Database\\Console\\Migrations\\FreshCommand' => 'Illuminate\\Database\\MigrationServiceProvider',
+    'Illuminate\\Database\\Console\\Migrations\\InstallCommand' => 'Illuminate\\Database\\MigrationServiceProvider',
+    'Illuminate\\Database\\Console\\Migrations\\RefreshCommand' => 'Illuminate\\Database\\MigrationServiceProvider',
+    'Illuminate\\Database\\Console\\Migrations\\ResetCommand' => 'Illuminate\\Database\\MigrationServiceProvider',
+    'Illuminate\\Database\\Console\\Migrations\\RollbackCommand' => 'Illuminate\\Database\\MigrationServiceProvider',
+    'Illuminate\\Database\\Console\\Migrations\\StatusCommand' => 'Illuminate\\Database\\MigrationServiceProvider',
+    'Illuminate\\Database\\Console\\Migrations\\MigrateMakeCommand' => 'Illuminate\\Database\\MigrationServiceProvider',
+    'composer' => 'Illuminate\\Foundation\\Providers\\ComposerServiceProvider',
     'Illuminate\\Contracts\\Pipeline\\Hub' => 'Illuminate\\Pipeline\\PipelineServiceProvider',
   ),
   'when' => 
@@ -48,6 +64,12 @@
     array (
     ),
     'Illuminate\\Cache\\CacheServiceProvider' => 
+    array (
+    ),
+    'Illuminate\\Database\\MigrationServiceProvider' => 
+    array (
+    ),
+    'Illuminate\\Foundation\\Providers\\ComposerServiceProvider' => 
     array (
     ),
     'Illuminate\\Pipeline\\PipelineServiceProvider' => 
