6014a87a5b50a8da8eb83577c55d57ff7e7db9ae
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-09-11 09:29:09 +0100

    Update video-page.blade.php

--- web/app/themes/wraith/resources/views/partials/flexible-content/video-page.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/video-page.blade.php
@@ -4,7 +4,7 @@ $title = get_sub_field('title');
 
 <section id="videosPage" class="">
   <div class="container mx-auto py-8 sm:py-12 lg:pt-20 mt-16 lg:mt-32">
-    <h1 class="text-primary lg:text-5xl text-4xl font-bold font-serif">{{ $title ? $title : null }}</h1>
+    <h1 class="text-primary uppercase font-bold font-serif tracking-wide text-3xl lg:text-4xl mb-12">{{ $title ? $title : null }}</h1>
   </div>
   <div class="bg-gray-100 py-12">
     <div class="container mx-auto">
