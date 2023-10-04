c8241423a9e7a56509f52bd3318040967b58efa2
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-10-04 16:21:50 +0100

    Update latest-news.blade.php

--- web/app/themes/wraith/resources/views/partials/flexible-content/latest-news.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/latest-news.blade.php
@@ -10,7 +10,7 @@ $i = 0;
   'post_type'      => 'post',
   'posts_per_page' => '4'
 ])
-<div id="latest-news" class="{!! $custom_class ? ' ' . $custom_class : ' ' !!}">
+<div id="latest-news" class="overflow-hidden {!! $custom_class ? ' ' . $custom_class : ' ' !!}">
   <div class="container mx-auto lg:flex">
     <div class="lg:w-1/2 w-full lg:mr-8 mr-0">
       <h2 class="text-primary md:text-4xl text-3xl font-serif font-bold max-w-[350px] mb-8 ">{{ $title }}</h2>
