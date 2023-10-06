bb0bf5e035df00ca8440b6cc583c4ecbf286a15c
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-10-06 11:21:09 +0100

    Update form-visit-showroom.blade.php

--- web/app/themes/wraith/resources/views/partials/flexible-content/form-visit-showroom.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/form-visit-showroom.blade.php
@@ -11,48 +11,48 @@ $details = get_sub_field('details');
         @if( $subtitle )
         <h4 class="title-display{{ $details ? null : ' text-center title-display-center' }} text-primary mb-4">{{ $subtitle }}</h4>
         @endif @if( $title )
-        <h2 class="text-4xl mb-8{{ $details ? null : ' text-center' }}">{{ $title }}</h2>
+        <h2 class="text-4xl mb-8 text-gray-700 font-bold {{ $details ? null : ' text-center' }}">{{ $title }}</h2>
         @endif
         <div class="flex flex-wrap">
           <div class="w-full{!! $details ? ' md:w-3/5 md:pr-8 mb-8 md:mb-0' : null !!}">
             <form class="icaal-contact-form" data-form="contact">
               <div class="flex flex-wrap">
                 <div class="w-full md:w-1/2 mb-2  md:pr-4">
-                  <div class="text-sm text-gray-700 pb-2">First Name*</div>
+                  <div class="font-bold text-gray-700 pb-2">First Name*</div>
                   <input name="first_name" type="text">
                   <div class="invalid-feedback text-red"></div>
                 </div>
                 <div class="w-full md:w-1/2 mb-2 md:pl-4">
-                  <div class="text-sm text-gray-700 pb-2">Last Name*</div>
+                  <div class="font-bold text-gray-700 pb-2">Last Name*</div>
                   <input name="last_name" type="text">
                   <div class="invalid-feedback text-red"></div>
                 </div>
                 <div class="w-full mb-2">
-                  <div class="text-sm text-gray-700 pb-2">Email Address*</div>
+                  <div class="font-bold text-gray-700 pb-2">Email Address*</div>
                   <input name="email" type="email">
                   <div class="invalid-feedback text-red"></div>
                 </div>
                 <div class="w-full md:w-1/2 mb-2 md:pr-4">
-                  <div class="text-sm text-gray-700 pb-2">Phone Number*</div>
+                  <div class="font-bold text-gray-700 pb-2">Phone Number*</div>
                   <input name="phone" type="tel">
                   <div class="invalid-feedback text-red"></div>
                 </div>
                 <div class="w-full md:w-1/2 mb-2 md:pl-4">
-                  <div class="text-sm text-gray-700 pb-2">Postcode</div>
+                  <div class="font-bold text-gray-700 pb-2">Postcode</div>
                   <input name="postcode" type="text">
                   <div class="invalid-feedback text-red"></div>
                 </div>
                 <div class="w-full mb-2">
-                  <div class="text-sm text-gray-700 pb-2">Message</div>
+                  <div class="font-bold text-gray-700 pb-2">Message</div>
                   <textarea name="message" placeholder="" rows="4"></textarea>
                 </div>
                 <div class="w-full md:w-1/2 mb-2 md:pr-4">
-                  <div class="text-sm text-gray-700 pb-2">Select Day</div>
+                  <div class="font-bold text-gray-700 pb-2">Select Day</div>
                   <input name="day" type="date">
                   <div class="invalid-feedback text-red"></div>
                 </div>
                   <div class="w-full md:w-1/2 mb-2 md:pl-4">
-                    <div class="text-sm text-gray-700 pb-2">Choose Time</div>
+                    <div class="font-bold text-gray-700 pb-2">Choose Time</div>
                     <select class="formField selection relative" name="time" placeholder="Time*">
                         <option disabled="" selected="" value="">Time*</option>
                         <option value="09:30">09:00</option>
