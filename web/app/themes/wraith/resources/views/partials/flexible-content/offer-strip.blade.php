1f537cbd31056187fe5daa9bdd79389adfae4b7b
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-10-12 17:00:01 +0100

    Update offer-strip.blade.php

--- web/app/themes/wraith/resources/views/partials/flexible-content/offer-strip.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/offer-strip.blade.php
@@ -13,7 +13,7 @@ $i = 0 ;
       $link = get_sub_field('link');
       
       @endphp
-      <div class="w-full py-4 px-8 text-center items-center {{ $i % 1 == 0 ? 'justify-start' : 'justify-center' }}  flex  {{ $i % 2 == 0 ? 'bg-primary offer-skew' : 'bg-secondary'  }}">
+      <div class="w-full py-4 px-8 text-center items-center {{ $i = 1 ? 'justify-start' : 'justify-center' }}  flex  {{ $i % 2 == 0 ? 'bg-primary offer-skew' : 'bg-secondary'  }}">
         <div class="{{ $i % 2 == 0 ? 'offer-skew-opp' : null }}">
           <h2 class="font-serif font-medium text-[25px] mx-auto text-white w-[70%] ">{{ $title }}</h2>
           <a href="{{ $link }}" class="hover:border-none text-[35px] font-bold font-serif text-white ">{{ $label }}</a>
