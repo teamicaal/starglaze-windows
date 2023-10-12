d8ebf2d2d7ed9a0de15f5f8b148aa03db2277cea
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-10-12 14:14:05 +0100

    Update ultraframe-cgi-block.blade.php

--- web/app/themes/wraith/resources/views/partials/flexible-content/ultraframe-cgi-block.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/ultraframe-cgi-block.blade.php
@@ -3,9 +3,9 @@
     $cgi_iframe = get_sub_field('cgi_iframe');
 @endphp
 
-<section class="container mx-auto">
+<section class="container mx-auto lg:mt-16 mt-10">
     <div>
-        <h2 class="lg:mt-16 mt-10  mb-4 lg:mb-8 text-primary font-serif font-medium text-4xl text-center">{{ $title }}</h2>
+        <h2 class="mb-4 lg:mb-8 text-primary font-serif font-medium text-4xl text-center">{{ $title }}</h2>
         <div>{!! $cgi_iframe !!}</div>
     </div>
     
