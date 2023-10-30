b0916e613843ac4f007b48210188c22ad292450d
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-10-30 17:15:45 +0000

    change

--- web/app/themes/wraith/resources/scripts/custom.js
+++ web/app/themes/wraith/resources/scripts/custom.js
@@ -289,7 +289,7 @@ import mixitup from 'mixitup';
   //why choose us slider
   $('.why-choose-us-slick').slick({
     slidesToShow: 3,
-    autoplay:false,
+    autoplay:true,
     autoplaySpeed:5000,
     dots:true,
     infinite:true,
