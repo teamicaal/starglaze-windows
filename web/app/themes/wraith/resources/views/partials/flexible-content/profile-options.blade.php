1df14d39e1b8e003ef3f48b16e3abc82f4c4a059
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-10-30 15:33:23 +0000

    change in profile block

--- web/app/themes/wraith/resources/views/partials/flexible-content/profile-options.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/profile-options.blade.php
@@ -2,13 +2,11 @@
     $title = get_sub_field('title');
     $paragraph = get_sub_field('paragraph');
 @endphp
-<div class="container mx-auto">
-    <div class="bg-[#F0F3F4] px-8 pt-8 pb-24">
-        <h2 class="text-center text-3xl font-bold font-serif text-primary lg:text-4xl mb-8 uppercase"> {{ $title }}</h2>
-        <div class="mb-0 child-p:font-normal child-p:text-[#3A3A3A]"> {!! $paragraph !!} </div>
-    </div>
+<section class="container mx-auto">
+    <h2 class="text-center text-3xl font-bold font-serif text-primary lg:text-4xl mb-8 uppercase"> {{ $title }}</h2>
+    <div class="child-p:mb-0 child-p:mx-auto child-p:font-normal child-p:text-[#3A3A3A]"> {!! $paragraph !!} </div>
     @if (have_rows('profile_options'))
-        <div class="flex justify-center gap-10">
+        <div class="flex justify-center gap-10 mt-10">
             @while (have_rows('profile_options'))
                 @php
                     the_row();
@@ -16,7 +14,7 @@
                     $profile_image = get_sub_field('profile_image');
                 @endphp
                 <div class="">
-                    <h3>{{ $profile_name }}</h3>
+                    <h3 class="text-center text-primary text-lg font-serif mb-4">{{ $profile_name }}</h3>
                     <div class="aspect-3/4 max-h-[500px] overflow-hidden">
                         <img class="w-full h-full object-cover hover:h-[110%] ease-in-out duration-500" src="{{ $profile_image['url'] }}" alt="{{ $profile_image['alt'] }}">
                     </div>
@@ -24,4 +22,4 @@
             @endwhile
         </div>
     @endif
-</div>
+    </section>
