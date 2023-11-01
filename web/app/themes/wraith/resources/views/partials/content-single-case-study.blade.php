5a4032f6ec3572f59060fb0badade68c1b28d5e4
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-11-01 09:48:01 +0000

    Update content-single-case-study.blade.php

--- web/app/themes/wraith/resources/views/partials/content-single-case-study.blade.php
+++ web/app/themes/wraith/resources/views/partials/content-single-case-study.blade.php
@@ -3,7 +3,7 @@
   <article @php post_class() @endphp>
       <div class="flex flex-col lg:flex-row justify-center mb-12 shadow-none">
           <div class="w-full lg:w-2/5 lg:my-auto mb-2 lg:mr-4 mr-0">
-              <h1 class="text-3xl md:text-4xl font-medium text-primary mb-8">{!! get_the_title() !!}</h1>
+              <h1 class="text-3xl md:text-4xl font-bold font-serif text-primary mb-8">{!! get_the_title() !!}</h1>
               <div class="flex flex-wrap">
                   <a class=" btn bg-secondary text-white hover:bg-primary hover:border-none block md:inline-block text-center  mb-4 mr-4 md:mb-0"
                       href="#article">Learn More</a>
