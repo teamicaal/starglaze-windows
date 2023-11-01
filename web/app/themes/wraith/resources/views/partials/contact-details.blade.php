949f77e7fde9d05580261217d3ab41f7f8eb2df3
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-11-01 14:25:59 +0000

    Update contact-details.blade.php

--- web/app/themes/wraith/resources/views/partials/contact-details.blade.php
+++ web/app/themes/wraith/resources/views/partials/contact-details.blade.php
@@ -79,7 +79,7 @@ $social_pinterest = get_field('social_pinterest', 'option');
       <div class="flex">
         <i class="fa fa-clock mr-3 mt-[0.4rem] {!! $colour == 'white' ? 'text-' . $colour   : 'text-primary' !!}"></i>
         <div>
-          <h2 class="text-2xl font-bold mb-1-5" {!! $colour == 'white' ? 'text-' . $colour   : 'text-primary' !!}>{!! $opening_hour_title !!}</h2>
+          <h2 class="text-2xl font-bold mb-1-5 {!! $colour == 'white' ? 'text-' . $colour   : 'text-primary' !!}">{!! $opening_hour_title !!}</h2>
           @if (have_rows('time', 'option'))
           @while(have_rows('time', 'option'))
                   @php
