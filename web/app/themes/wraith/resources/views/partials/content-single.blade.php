43782400ef89618abb91689d152dca0ec7045c3a
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-11-01 09:59:20 +0000

    change text-colour

--- web/app/themes/wraith/resources/views/partials/content-single.blade.php
+++ web/app/themes/wraith/resources/views/partials/content-single.blade.php
@@ -3,7 +3,7 @@
   <article @php post_class() @endphp>
       <div class="flex flex-col lg:flex-row justify-center mb-12 shadow-none">
           <div class="w-full lg:w-2/5 lg:my-auto mb-2">
-              <h1 class="text-3xl md:text-4xl text-secondary font-semibold mb-8">{!! get_the_title() !!}</h1>
+              <h1 class="text-3xl md:text-4xl text-secondary font-bold font-serif mb-8">{!! get_the_title() !!}</h1>
               <div class="flex flex-wrap">
                   <a class="btn block md:inline-block bg-primary text-white hover:bg-secondary hover:border-none mb-4 md:mr-4 md:mb-0"
                       href="#article">Learn More</a>
