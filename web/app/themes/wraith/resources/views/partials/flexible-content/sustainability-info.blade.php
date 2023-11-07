6a2528a9b9fc225aa2df8cdd6a4cba1fa94f77dd
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-11-07 11:22:33 +0000

    Update sustainability-info.blade.php

--- web/app/themes/wraith/resources/views/partials/flexible-content/sustainability-info.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/sustainability-info.blade.php
@@ -10,7 +10,7 @@ $i = 0;
 <section id="sustainability-info" class="relative {!! $custom_class ? ' ' . $custom_class : 'lg:py-12 py-8' !!}">
     <div class="container mx-auto">
         <div class="flex relative">
-            {!! $title ? '<h2 class="text-2xl md:text-4xl mb-10 text-[#3F7E57] md:w-2/3 w-full tracking-wide font-bold font-serif">' . $title . '</h2>' : null !!}
+            {!! $title ? '<h2 class="text-2xl text-center md:text-4xl mb-10 text-[#3F7E57] md:w-2/3 w-full tracking-wide font-bold font-serif">' . $title . '</h2>' : null !!}
             <img src="{{ $logo_image['url'] }}" alt="{{ $logo_image['alt'] }}" class="absolute right-[13rem] top-[-2rem]">
             <a href="{!! $button_link !!}" class="btn bg-primary hover:scale-105 transition-all ease-in-out duration-500 text-white absolute md:inline-block block right-0 bottom-0">{!! $button_label !!}</a>
         </div>
