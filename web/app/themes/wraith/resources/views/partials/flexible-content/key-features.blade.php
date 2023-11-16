0a9062cc3c7b437d9f3cbd14615d24d1490c4131
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-11-16 12:22:26 +0000

    Update key-features.blade.php

--- web/app/themes/wraith/resources/views/partials/flexible-content/key-features.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/key-features.blade.php
@@ -5,7 +5,7 @@
 
 <section class="py-12 bg-gray-100">
   <div class="container mx-auto">
-      {!! $title ? '<h2 class="mb-4 lg:mb-8 text-primary text-center font-serif font-medium text-4xl">' . $title . '</h2>' : null !!}
+    <h2 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="100" class="mb-4 lg:mb-8 text-primary text-center font-serif font-medium text-4xl">{!! $title !!}</h2>
       <div class="flex flex-wrap justify-center">
         @if( get_sub_field('features') )
           @php $count = count(get_sub_field('features')); @endphp
@@ -20,7 +20,7 @@
           @endphp
 
             <div class="w-1/2 md:w-1/3{{ $count > 3 ? ' lg:w-1/4' : null }}">
-              <div class="blue-bg-gradient rounded-sm shadow-card p-4 lg:p-8 mx-4 mb-8 lg:mb-4 text-center">
+              <div data-aos="flip-up" data-aos-duration="1000" data-aos-delay="100" class="blue-bg-gradient rounded-sm shadow-card p-4 lg:p-8 mx-4 mb-8 lg:mb-4 text-center">
                 @if( $icon )
                   <span class="block mx-auto bg-primary rounded-full w-12 h-12 flex justify-center items-center">
                     <i class="fa fa-{{ $icon }} text-white text-xl"></i>
