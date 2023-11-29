6541fb82db951044f83602b70b646e78c5568274
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-11-29 09:39:47 +0000

    change a style

--- web/app/themes/wraith/resources/views/partials/flexible-content/contact-content.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/contact-content.blade.php
@@ -8,6 +8,6 @@
 <section id="contact-content" class="py-12">
   <div class="container mx-auto">
     {!! $title ? '<h2 class="text-primary text-center capitalize font-bold font-serif tracking-wide text-3xl lg:text-5xl mb-4">' . $title . '</h2>' : null !!}
-    <div class="md:py-8 child-p:mx-auto child-p:font-normal child-p:text-center child-p:text-[#3A3A3A]">{!! $paragraph !!} </div>
+    <div class="md:pt-8 child-p:mx-auto child-p:font-normal child-p:text-center child-p:text-[#3A3A3A]">{!! $paragraph !!} </div>
   </div>
 </section>
