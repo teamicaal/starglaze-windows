c336f56093acdd04dbe9e37d3453676c2255df41
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-11-29 13:06:58 +0000

    Update all-forms.blade.php

--- web/app/themes/wraith/resources/views/partials/flexible-content/all-forms.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/all-forms.blade.php
@@ -16,11 +16,12 @@ $details = get_sub_field('details');
                 the_row();
                 $form_icon = get_sub_field('form_icon');
                 $form_title = get_sub_field('form_title');
-                $form_link = get_sub_field('form_link');
+                $form_custom_link = get_sub_field('form_custom_link');
+                $from_page_link = get_sub_field('from_page_link');
                 $i++;
               @endphp
 
-                  <a href="{{ $form_link }}" class="lg:w-[30%] w-full rounded-lg max-h-[170px] mb-6 lg:mr-6 text-center flex flex-col justify-center items-center bg-white hover:bg-primary group hover:border-none border-none">
+                  <a href="{!! $form_custom_link ? $form_custom_link : get_the_permalink( $from_page_link ) !!}" class="lg:w-[30%] w-full rounded-lg max-h-[170px] mb-6 lg:mr-6 text-center flex flex-col justify-center items-center bg-white hover:bg-primary group hover:border-none border-none">
                     <i class="text-4xl text-[#8a8f92] h-[40px] w-[40px] group-hover:text-white rounded-full p-4 fas fa-{!! $form_icon !!}"></i>
                     <h3 class="text-[#8a8f92] text-lg font-serif  group-hover:text-white  tracking-wide font-bold">{{ $form_title }}</h3>
                   </a>
