99f2b8e9998a5eacb537817e0cb1c55e1dcf6d49
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-12-19 09:39:54 +0000

    Update latest-news.blade.php

--- web/app/themes/wraith/resources/views/partials/flexible-content/latest-news.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/latest-news.blade.php
@@ -58,8 +58,8 @@ $i = 0;
                 </button>
                 <div class="flex flex-col w-4/5">
                   <a href="{{ $button_link }}" class="border-none">
-                    <h2 class="text-white md:text-6xl text-5xl font-bold tracking-wide">{{ $top_text_line }}</h2>
-                    <h3 class="text-white md:text-3xl text-3xl font-bold tracking-wide">{{ $bottom_text_line }}</h3>
+                    <h2 class="text-white md:text-6xl text-3xl font-bold tracking-wide">{{ $top_text_line }}</h2>
+                    <h3 class="text-white md:text-3xl text-xl font-bold tracking-wide">{{ $bottom_text_line }}</h3>
                   </a>
                 </div>
                
