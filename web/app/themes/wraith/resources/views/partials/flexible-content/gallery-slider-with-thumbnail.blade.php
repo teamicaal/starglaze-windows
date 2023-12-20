58866eae42213a944fe6ec147bdf736d2ec4d21b
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-12-20 15:31:37 +0000

    change in blocks

--- web/app/themes/wraith/resources/views/partials/flexible-content/gallery-slider-with-thumbnail.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/gallery-slider-with-thumbnail.blade.php
@@ -5,8 +5,8 @@
   $images = get_sub_field('gallery_items');
 @endphp
 
-<section class="gallery-slider-with-thumb {!! $custom_class ? ' ' . $custom_class : 'py-12'; !!}">
-  <div class="container mx-auto relative">
+<section class="gallery-slider-with-thumb {!! $custom_class ? ' ' . $custom_class : 'py-12'; !!} overflow-hidden">
+  <div class="container mx-auto relative overflow-hidden">
   <h2 data-aos="fade-left" data-aos-duration="1000" data-aos-delay="100" class="text-center lg:text-4xl text-3xl font-bold font-serif text-primary mb-8">{!! $title !!}</h2>
   <div data-aos="fade-right" data-aos-duration="1000" data-aos-delay="100" class="child-p:mb-12 child-p:mx-auto child-p:text-center child-p:text-[#3A3A3A] child-p:leading-loose">{!! $paragraph !!}</div>
 
@@ -29,7 +29,7 @@
       <button class="hidden lg:flex slick-p absolute group justify-center items-center left-0 top-1/2 -translate-y-1/2 -translate-x-full  px-6 py-4 bg-primary p-">
           <i class="group-hover:rotate-[360deg] text-[26px] fa-solid fa-chevron-left text-white transition-all ease-in-out duration-500"></i>
       </button> --}}
-      <div  class="container mx-auto relative">
+      <div  class="container mx-auto relative overflow-hidden">
         <div class="image_slider_thmb">
           @php
               $i = 0;
--- web/app/themes/wraith/resources/views/partials/flexible-content/virtual-showroom.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/virtual-showroom.blade.php
@@ -4,7 +4,7 @@ $paragraph = get_sub_field('paragraph');
 $iframe = get_sub_field('iframe');
 @endphp 
 
-<section id="virtual-showroom" class=" py-12">
+<section id="virtual-showroom" class=" py-12 overflow-hidden">
   <h1 data-aos="fade-right" data-aos-duration="1000" data-aos-delay="100" class="text-primary lg:text-4xl text-3xl font-bold text-center font-serif mb-6">{{ $title ? $title : null }}</h1>
   <div data-aos="fade-left" data-aos-duration="1000" data-aos-delay="100" class="mb-1 child-p:relative child-p:mx-auto child-p:text-[#3A3A3A] child-p:font-normal child-p:max-w-screen-md child-p:leading-7 child-p:text-center child-p:text-sm">{!! $paragraph !!}</div>
   <div data-aos="fade-down" data-aos-duration="1000" data-aos-delay="100" class="container mx-auto">
